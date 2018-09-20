dfdedf65339bb603a64af4063ef2e07a70e7978b
jed <jed+jupyter_NotebookCleaners@m6700-LINUX>
2018-09-19 20:07:47 -0400

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,32 @@
+import setuptools
 from setuptools import setup
+
+import os
+LOCAL_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def read_requirements(path="requirements.txt"):
+    requirements = []
+
+    with open(os.path.join(LOCAL_DIR, "requirements.txt"), "r") as infile:
+        for line in infile:
+            line = line.strip()
+            if line and not line[0] == "#":  # ignore comments
+                requirements.append(line)
+    return requirements
+
+requirements = read_requirements()
+
 
 setup(
     name='notebookcleaner',
+    description="Cleanup Jupyter Notebooks",
+    author="Jed",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=requirements,
+    include_package_data=True,
     entry_points={
         'nbc_cleaners': [
             'bubble_imports = notebookcleaner:bubble_notebook_imports',
